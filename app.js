b3ad573fa1b3a80df4227fa0cf3f5e64cbcb3c41
KasRoid <KasRoid@gmail.com>
2021-05-15 23:53:23 +0900

    feat: Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -24,14 +24,15 @@ function rqListener(req, res) {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on(`end`, () => {
+    return req.on(`end`, () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split(`=`)[1];
-      fs.writeFileSync(`message.txt`, message);
+      fs.writeFile(`message.txt`, message, (error) => {
+        res.statusCode = 302;
+        res.setHeader(`Location`, `/`);
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader(`Location`, `/`);
-    return res.end();
   }
   res.setHeader(`Content-Type`, `text/html`);
   res.write(`<html>`);
