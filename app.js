2b1cd24999ffaef7b23f30b64f95ca915128c073
KasRoid <KasRoid@gmail.com>
2021-05-15 23:25:04 +0900

    feat: Redirecting Requests

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const http = require(`http`);
+const fs = require(`fs`);
+
 const server = http.createServer(rqListener);
 
 function rqListener(req, res) {
   console.log(req.url, req.method, req.headers);
   //   process.exit();
   const url = req.url;
+  const method = req.method;
+
   if (url == `/`) {
     res.write(`<html>`);
     res.write(`<head><title>MyEnter Message</title><head>`);
@@ -12,6 +16,12 @@ function rqListener(req, res) {
       `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
     );
     res.write(`</html>`);
+    return res.end();
+  }
+  if (url === `/message` && method === `POST`) {
+    fs.writeFileSync(`message.txt`, `Dummy`);
+    res.statusCode = 302;
+    res.setHeader(`Location`, `/`);
     return res.end();
   }
   res.setHeader(`Content-Type`, `text/html`);
