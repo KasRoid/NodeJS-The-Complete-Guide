f69a75ab15c1dffc7474653a6d19c7c76faea5fd
KasRoid <KasRoid@gmail.com>
2021-05-25 16:43:24 +0900

    feat: Assignment 4 - Implemented EJS

--- Working with Express.js/Assignment4/app.js
+++ Working with Express.js/Assignment4/app.js
@@ -1,22 +1,13 @@
 const path = require(`path`);
 
 const express = require(`express`);
-const expressHbs = require(`express-handlebars`);
 
 const app = express();
 
 const homeData = require(`./routes/home`);
 const usersRoutes = require(`./routes/users`);
-const { extname } = require("path");
 
-app.engine(
-  `hbs`,
-  expressHbs({
-    extname: `hbs`,
-    defaultLayout: false,
-  })
-);
-app.set(`view engine`, `hbs`);
+app.set(`view engine`, `ejs`);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, `public`)));
