fad95c439acb25bbe4837c327fd2b09837bfbc97
KasRoid <KasRoid@gmail.com>
2021-05-25 13:21:18 +0900

    feat: Assignment 4 - Implemented Handlebars

--- Working with Express.js/Assignment4/app.js
+++ Working with Express.js/Assignment4/app.js
@@ -1,13 +1,22 @@
 const path = require(`path`);
 
 const express = require(`express`);
+const expressHbs = require(`express-handlebars`);
 
 const app = express();
 
 const homeData = require(`./routes/home`);
 const usersRoutes = require(`./routes/users`);
+const { extname } = require("path");
 
-app.set(`view engine`, `pug`);
+app.engine(
+  `hbs`,
+  expressHbs({
+    extname: `hbs`,
+    defaultLayout: false,
+  })
+);
+app.set(`view engine`, `hbs`);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, `public`)));
