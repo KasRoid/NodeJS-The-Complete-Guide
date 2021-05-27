2f5dbe38893a70c0e9a041b02605763c0c60465a
KasRoid <KasRoid@gmail.com>
2021-05-27 15:42:12 +0900

    feat: Registering the Routes

--- Working with Express.js/Lecture/models/product.js
+++ Working with Express.js/Lecture/models/product.js
@@ -1,6 +1,5 @@
 const fs = require(`fs`);
 const path = require(`path`);
-const { getProducts } = require("../controllers/products");
 
 const filePath = path.join(
   path.dirname(require.main.filename),
