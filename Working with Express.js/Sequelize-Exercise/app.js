f7fe3129b401120b3ab7343295c04d209d587866
KasRoid <KasRoid@gmail.com>
2021-06-16 17:06:06 +0900

    UI: Setting Up Home Page

--- Working with Express.js/Sequelize-Exercise/app.js
+++ Working with Express.js/Sequelize-Exercise/app.js
@@ -1,6 +1,14 @@
+const path = require(`path`);
+
 const express = require(`express`);
 
 const app = express();
+
+const shopRouter = require(`./Routers/shopRouter`);
+
+app.use(express.static(path.join(__dirname, `public`)));
+
+app.use("/", shopRouter);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
