34a3db887ffd421f1fd6bb3771d4bc5d26e1cd8e
KasRoid <KasRoid@gmail.com>
2021-06-18 15:36:16 +0900

    UI: Created Home View

--- Working with Express.js/Sequelize-Exercise/public/js/script.js
+++ Working with Express.js/Sequelize-Exercise/public/js/script.js
@@ -1,18 +1,21 @@
 const makeCardList = () => {
-  const names = [`Kas Song`, `Michael`];
-  const jobs = [`iOS Developer`, `Engineer`];
+  const names = [`Song`, `David`];
+  const jobs = [`Developer`, `Bad Engineer`];
 
   names.forEach(function (name, index) {
     const img = document.createElement(`img`);
     img.src = `https://www.vhv.rs/dpng/d/544-5445462_people-icons-png-flat-person-icon-png-transparent.png`;
     img.alt = `Avatar`;
-    img.style = `width: 10%`;
+    img.style = `width: 10vw`;
     const h4 = document.createElement(`h4`);
     const b = document.createElement(`b`);
+    b.id = `title`;
     const p = document.createElement(`p`);
+    p.id = `description`;
     const cardDiv = document.createElement(`div`);
     const infoDiv = document.createElement(`div`);
-    cardDiv.className = `card${index}`;
+    infoDiv.className = `info`;
+    cardDiv.className = `card`;
     b.innerHTML += name;
     p.innerHTML += jobs[index];
     h4.appendChild(b);
