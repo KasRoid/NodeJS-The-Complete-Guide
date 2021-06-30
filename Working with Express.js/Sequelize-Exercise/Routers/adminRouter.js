57ea8b52b611669b32cf9e8392966009043cf552
KasRoid <KasRoid@gmail.com>
2021-06-30 09:32:49 +0900

    fix: Now Product ID sets by Random Number instead of Array Length.

--- Working with Express.js/Sequelize-Exercise/Routers/adminRouter.js
+++ Working with Express.js/Sequelize-Exercise/Routers/adminRouter.js
@@ -6,13 +6,6 @@ const router = express.Router();
 
 const Product = require(`../models/product`);
 
-const createProductId = (callback) => {
-  Product.fetchAll((products) => {
-    const id = products.length + 1;
-    callback(id);
-  });
-};
-
 router.get(`/`, (req, res, next) => {
   res.sendFile(path.join(__dirname, `..`, `views`, `admin.html`));
 });
@@ -22,14 +15,13 @@ router.get(`/edit`, (req, res, netx) => {
 });
 
 router.post(`/`, (req, res, next) => {
-  createProductId((id) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    const imageURL = req.body.imageURL;
-    const product = new Product(id, title, description, imageURL);
-    product.save();
-    res.redirect(`/`);
-  });
+  const id = Math.random();
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageURL = req.body.imageURL;
+  const product = new Product(id, title, description, imageURL);
+  product.save();
+  res.redirect(`/`);
 });
 
 router.post(`/delete`, (req, res, next) => {
