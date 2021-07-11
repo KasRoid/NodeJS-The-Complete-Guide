743fde662abc1be05bfc970d4ca5b6cf6c3dc2d9
KasRoid <KasRoid@gmail.com>
2021-07-11 16:15:13 +0900

    feat: Implemented Controller to Admin Routers

--- Working with Express.js/Sequelize-Exercise/Controllers/adminController.js
+++ Working with Express.js/Sequelize-Exercise/Controllers/adminController.js
@@ -1,1 +1,32 @@
+const path = require(`path`);
 
+const Product = require(`../models/product`);
+
+exports.getAddProduct = (req, res, next) => {
+  res.sendFile(path.join(__dirname, `..`, `views`, `admin.html`));
+};
+
+exports.getEditProduct = (req, res, netx) => {
+  res.sendFile(path.join(__dirname, `..`, `views`, `admin-edit.html`));
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const id = Math.random();
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageURL = req.body.imageURL;
+  const product = new Product(id, title, description, imageURL);
+  product.save();
+  res.redirect(`/`);
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const id = req.body.productID;
+  Product.delete(id);
+  res.redirect(`/admin/edit`);
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const id = req.body.productID;
+  res.sendFile(path.join(__dirname, `..`, `views`, `admin-edit-item.html`));
+};
--- Working with Express.js/Sequelize-Exercise/Routers/adminRouter.js
+++ Working with Express.js/Sequelize-Exercise/Routers/adminRouter.js
@@ -1,40 +1,16 @@
-const path = require(`path`);
-
 const express = require(`express`);
 
 const router = express.Router();
 
-const Product = require(`../models/product`);
+const controller = require(`../controllers/adminController`);
 
 // GET
-router.get(`/`, (req, res, next) => {
-  res.sendFile(path.join(__dirname, `..`, `views`, `admin.html`));
-});
-
-router.get(`/edit`, (req, res, netx) => {
-  res.sendFile(path.join(__dirname, `..`, `views`, `admin-edit.html`));
-});
+router.get(`/`, controller.getAddProduct);
+router.get(`/edit`, controller.getEditProduct);
 
 // POST
-router.post(`/`, (req, res, next) => {
-  const id = Math.random();
-  const title = req.body.title;
-  const description = req.body.description;
-  const imageURL = req.body.imageURL;
-  const product = new Product(id, title, description, imageURL);
-  product.save();
-  res.redirect(`/`);
-});
-
-router.post(`/delete`, (req, res, next) => {
-  const id = req.body.productID;
-  Product.delete(id);
-  res.redirect(`/admin/edit`);
-});
-
-router.post(`/edit/item`, (req, res, next) => {
-  const id = req.body.productID;
-  res.sendFile(path.join(__dirname, `..`, `views`, `admin-edit-item.html`));
-});
+router.post(`/`, controller.postAddProduct);
+router.post(`/delete`, controller.postDeleteProduct);
+router.post(`/edit/item`, controller.postEditProduct);
 
 module.exports = router;
